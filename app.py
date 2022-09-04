7de9f99c270ff1141490912db4d2ed995f8da171
easin arafat <easinarafat.jp@gmail.com>
2022-09-04 19:22:15 +0900

    init

---
+++ app.py
@@ -0,0 +1,21 @@
+print("easin arafat")
+print(322)
+easin=(12+12)
+print(easin)
+
+age=input("what is your age?")
+user_age=int(age)
+if(user_age >= 60):
+    print("you are older the adult")
+elif(user_age >=18):
+    print("you are adult")
+elif(user_age<10):
+    print("you are very child")
+else:
+    print ("you are teen")
+
+
+
+
+
+
